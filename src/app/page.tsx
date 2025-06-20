d68e9e243532240d9d5fd97b1ac7ab44d61cb5b1
MartinMuru <mutwiri333@gmail.com>
2025-06-20 09:05:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
 import Faqs from "@/sections/Faqs";
-import CallToAction from "@/sections/CallToAction";
 import Footer from "@/sections/Footer";
 
 export default function Home() {
