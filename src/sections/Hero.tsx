fcbdf59ecd9412f5b8e4f7c6d022512752b68ad3
MartinMuru <mutwiri333@gmail.com>
2025-07-02 20:40:58 +0100

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -94,7 +94,7 @@ export default function Hero() {
             <div className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
     <input 
         type="email" 
-        placeholder="Enter your email" 
+        placeholder="FinSquad.ai" 
         className="bg-transparent px-4 md:flex-1 w-full"
         disabled
     />
